682c31480cb54880fc8bbe3e9d02db70443af1ea
pericles001 <periclesadjovi@outlook.fr>
2021-11-18 03:08:39 +0100

    Format code

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,5 +2,5 @@
 """Print the alphabet in reverse order alterning upper and lower case"""
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
-	print("{}".format(chr(c - i)), end="")
-	i = 32 if i == 0 else 0
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
