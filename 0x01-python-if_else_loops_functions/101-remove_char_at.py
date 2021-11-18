35ad8728fbfbaf1ce3bbb83bc9c1196c22753aba
pericles001 <periclesadjovi@outlook.fr>
2021-11-18 02:59:08 +0100

    Correct code base

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,5 +3,4 @@ def remove_char_at(str, n):
     """Create a copy of the string without the character at position n"""
     if n < 0:
         return (str)
-    return (str[:n] + str[n+1:]
-
+    return (str[:n] + str[n+1:])
--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -4,6 +4,5 @@ def magic_calculation(a, b, c):
     if a < b:
         return (c)
     if c > b:
-        return (a +b)
+        return (a + b)
     return (a*b - c)
-
