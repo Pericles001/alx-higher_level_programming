99a08836d8caf829bcf054c6618706dcd019040d
pericles001 <periclesadjovi@outlook.fr>
2021-11-18 01:00:47 +0100

    Update code base

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for number in range(0, 100):
     if number == 99:
         print("{}".format(number))
     else:
-        print("{:02}".format(number), end="")
+        print("{:02}".format(number), end=", ")
