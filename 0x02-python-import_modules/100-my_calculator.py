a934eca801c2016d129098e14ae213322c19997a
pericles001 <periclesadjovi@outlook.fr>
2021-11-20 00:58:41 +0100

    Update document

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-
 if __name__ == "__main__":
-    """Handle basic arithmetic operations"""
+    """Handle basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
     import sys
 
