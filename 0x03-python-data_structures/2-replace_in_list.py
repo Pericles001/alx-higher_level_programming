02a52c2a5707c7eb3f503925645938c448abff2c
pericles001 <periclesadjovi@outlook.fr>
2021-11-22 00:48:25 +0100

    Update code base

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if idx >= 0 and idx < (len(my_list) - 1):
+    if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
     return (my_list)
