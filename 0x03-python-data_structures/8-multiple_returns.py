27184fe8e5e24b1b8e8a1e24e0c1629a71dc73d3
pericles001 <periclesadjovi@outlook.fr>
2021-11-23 00:35:04 +0100

    Update code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is "":
-        return len(sentence), 'None'
-    else:
-        return len(sentence), sentence[0]
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
