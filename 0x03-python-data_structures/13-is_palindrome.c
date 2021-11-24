9742a934bb7440fd29c743575466d2e2f0725812
pericles001 <periclesadjovi@outlook.fr>
2021-11-24 03:07:27 +0100

    CHange code base

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,39 +1,114 @@
 #include "lists.h"
 
 /**
- *is_palindrome - This checks if a singly linked list is a palindrome
- *@head: pointer to pointer
+ * reverse - reverses the second half of the list
  *
- *Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * @h_r: head of the second half
+ * Return: no return
  */
+void reverse(listint_t **h_r)
+{
+    listint_t *prv;
+    listint_t *crr;
+    listint_t *nxt;
 
+    prv = NULL;
+    crr = *h_r;
+
+    while (crr != NULL)
+    {
+        nxt = crr->next;
+        crr->next = prv;
+        prv = crr;
+        crr = nxt;
+    }
+
+    *h_r = prv;
+}
+
+/**
+ * compare - compares each int of the list
+ *
+ * @h1: head of the first half
+ * @h2: head of the second half
+ * Return: 1 if are equals, 0 if not
+ */
+int compare(listint_t *h1, listint_t *h2)
+{
+    listint_t *tmp1;
+    listint_t *tmp2;
+
+    tmp1 = h1;
+    tmp2 = h2;
+
+    while (tmp1 != NULL && tmp2 != NULL)
+    {
+        if (tmp1->n == tmp2->n)
+        {
+            tmp1 = tmp1->next;
+            tmp2 = tmp2->next;
+        }
+        else
+        {
+            return (0);
+        }
+    }
+
+    if (tmp1 == NULL && tmp2 == NULL)
+    {
+        return (1);
+    }
+
+    return (0);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list
+ * is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 if it is not a palindrome,
+ * 1 if it is a palndrome
+ */
 int is_palindrome(listint_t **head)
 {
-    int list[1000000], i = 0, j = 0, k = 0;
-    listint_t *temp;
+    listint_t *slow, *fast, *prev_slow;
+    listint_t *scn_half, *middle;
+    int isp;
 
-    if (head == NULL || (*head) == NULL)
-        return (1);
-    temp = *head;
-    while (temp)
+    slow = fast = prev_slow = *head;
+    middle = NULL;
+    isp = 1;
+
+    if (*head != NULL && (*head)->next != NULL)
     {
-        list[i] = temp->n;
-        i++;
-        temp = temp->next;
-    }
-    i--;
+        while (fast != NULL && fast->next != NULL)
+        {
+            fast = fast->next->next;
+            prev_slow = slow;
+            slow = slow->next;
+        }
 
-    if (i % 2 != 0)
-        k = (i + 1) / 2;
-    else
-        k = i / 2;
+        if (fast != NULL)
+        {
+            middle = slow;
+            slow = slow->next;
+        }
 
-    while (j < k)
-    {
-        if (list[j] != list[i])
-            return (0);
-        i--;
-        j++;
+        scn_half = slow;
+        prev_slow->next = NULL;
+        reverse(&scn_half);
+        isp = compare(*head, scn_half);
+
+        if (middle != NULL)
+        {
+            prev_slow->next = middle;
+            middle->next = scn_half;
+        }
+        else
+        {
+            prev_slow->next = scn_half;
+        }
     }
-    return (1);
+
+    return (isp);
 }
