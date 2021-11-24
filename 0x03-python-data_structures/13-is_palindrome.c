9e1cc8a129aa79d22a02e7a40595da760fa261c1
pericles001 <periclesadjovi@outlook.fr>
2021-11-24 02:33:47 +0100

    Update code

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ *is_palindrome - This checks if a singly linked list is a palindrome
+ *@head: pointer to pointer
+ *
+ *Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int list[1000000], i = 0, j = 0, k = 0;
+	listint_t *temp;
+
+	if (head == NULL || (*head) == NULL)
+		return (1);
+	temp = *head;
+	while (temp)
+	{
+		list[i] = temp->n;
+		i++;
+		temp = temp->next;
+	}
+	i--;
+
+	if (i % 2 != 0)
+		k = (i + 1) / 2;
+	else
+		k = i / 2;
+
+	while (j < k)
+	{
+		if (list[j] != list[i])
+			return (0);
+		i--;
+		j++;
+	}
+	return (1);
+}
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stdio.h>
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
