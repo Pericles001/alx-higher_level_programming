e16db36dbc7dec5c8b5fcf44ff000f89ee2b9e44
pericles001 <periclesadjovi@outlook.fr>
2021-11-25 19:25:28 +0100

    Submit solution

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    """adds all unique integers in a list (only once for each integer)."""
+    uniq_list = []
+
+    for i in my_list:
+        if i not in uniq_list:
+            uniq_list.append()
+    return lambda result: sum(uniq_list)
+
+    print("Result: {:d}".format(result))
