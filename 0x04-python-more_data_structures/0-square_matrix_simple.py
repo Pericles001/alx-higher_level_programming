8f7d402642559d74b56af4dbb579905acd598490
Pericles Adjovi <55035365+Pericles001@users.noreply.github.com>
2021-11-25 01:56:33 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,4 +5,4 @@ def square_matrix_simple(matrix=[]):
 def square_matrix_simple(matrix=[]):
     if not matrix:
         return None
-    return list(list(map(lambda a: a*a, num_list)) for num_list in matrix)
+    return (list(list(map(lambda a: a*a, num_list)) for num_list in matrix))
