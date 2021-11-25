bbdec5dbfe356ba8960aeeddde8df93f3e53c3c3
pericles001 <periclesadjovi@outlook.fr>
2021-11-25 15:32:26 +0100

    Update code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,4 +3,4 @@
 def search_replace(my_list, search, replace):
     if not my_list:
         return my_list
-    return (val if val != search else replace for val in my_list)
+    return [val if val != search else replace for val in my_list]
