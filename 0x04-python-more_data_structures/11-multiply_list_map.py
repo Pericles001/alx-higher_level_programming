3cca7d7936b23053c616d15ab2b7be88c0031e7c
pericles001 <periclesadjovi@outlook.fr>
2021-11-26 00:17:12 +0100

    Remove description

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-#  """returns a list with all values multiplied"""
-#    """ by a number without using any loops."""
 def multiply_list_map(my_list=[], number=0):
     return list(map(lambda x: x * number, my_list))
