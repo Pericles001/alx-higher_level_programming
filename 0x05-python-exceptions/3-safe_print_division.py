c8535e2da705e7029908d9c2ed195f0b993f8c78
pericles001 <periclesadjovi@outlook.fr>
2021-12-07 22:17:42 +0100

    Format file

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-       result = a/b
-    except :
+        result = a/b
+    except:
         result = None
     finally:
         print("Inside result: {}".format(result))
