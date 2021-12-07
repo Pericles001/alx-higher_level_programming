66dc5a456a3a982209465a4664f003e857c36ec2
pericles001 <periclesadjovi@outlook.fr>
2021-12-07 23:09:20 +0100

    Update code

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,7 +2,7 @@
 def safe_print_integer_err(value):
     import sys
     try:
-        print("{:d}\n".format(i))
+        print("{:d}".format(i))
     except Exception as i:
         sys.stderr.write("Exception: {}\n".format(i))
         return (False)
