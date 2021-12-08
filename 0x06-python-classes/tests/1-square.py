dc763d6a4d71a85ea7d8bed75aa369085223e5be
pericles001 <periclesadjovi@outlook.fr>
2021-12-08 16:07:37 +0100

    Update file

--- 0x06-python-classes/tests/1-square.py
+++ 0x06-python-classes/tests/1-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Square class to represent a square"""
+""" Working on oop with python """
 
 
 class Square():
-    """ initialize the class"""
+    """Class representing a square"""
 
-    def init(self, size):
-        """  Args:
-              size (int): size of the square
-       """
-    Self.__size = size
+    def __init__(self, size):
+        """ initialize the class
+
+        Args:
+           size (int): size of the square
+        """
+        self.__size = size
