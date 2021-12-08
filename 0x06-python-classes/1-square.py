06bcdadbef3aa385cc54b7c1af12894b671d153c
pericles001 <periclesadjovi@outlook.fr>
2021-12-08 15:57:58 +0100

    Update code

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square class to represent a square"""
+
+class Square():
+    """ initialize the class
+
+        Args:
+           size (int): size of the square
+    """
+    self.__size = size
