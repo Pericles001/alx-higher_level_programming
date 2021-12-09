262196fb08722ae65be8f7e63b6fb5d630801ca9
pericles001 <periclesadjovi@outlook.fr>
2021-12-09 20:52:16 +0100

    Update files

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,7 +18,7 @@ class Square():
             raise TypeError("size must be an integer")
         elif (value < 0):
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         return self.__size ** 2
