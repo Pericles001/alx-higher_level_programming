f2630478aa49136ba27a01db5c3738ad89a91568
pericles001 <periclesadjovi@outlook.fr>
2021-12-17 15:45:18 +0100

    Add class documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,6 +6,7 @@ Module for a class that prevents dynamic attributes creation
 
 
 class LockedClass():
+    """Class to prevent dynamic attributes creation"""
     __slots__ = ['first_name']
 
     def __init__(self):
