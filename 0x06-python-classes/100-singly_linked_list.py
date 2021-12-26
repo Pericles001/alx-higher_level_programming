61095bbaea3958929de22af09e72e300247d711a
Pericles001 <periclesadjovi@gmail.com>
2021-12-26 16:12:43 +0100

    Done with singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -45,3 +45,34 @@ class SinglyLinkedList:
     def __init__(self):
         """Initialize a new singly linked list"""
         self.__head = None
+
+    def sorted_insert(self, value):
+        """Insert a new Node to the singly linked list
+        
+            Args:
+                value(Node): The new node to insert
+        
+        """
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while(tmp.next_node is not None and tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
+
+
+    def __str__(self):
+        """Define the print() representation of a singly linked list"""
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(values))
