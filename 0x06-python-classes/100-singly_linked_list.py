052fc0f5cd260f16df165543d8f5b3366d77cda6
Pericles001 <periclesadjovi@gmail.com>
2021-12-26 15:59:41 +0100

    Set space and start class singy linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -14,12 +14,12 @@ class Node:
         
         self.data = data
         self.next_node = next_node
-        
+
     @property
     def data(self):
         """Getter or setter for the data of node"""
         return (self.__data)
-    
+
     @data.setter
     def data(self, value):
         if not isinstance(value, int):
@@ -31,5 +31,17 @@ class Node:
     def next_node(self):
         """Getter or setter for the next node"""
         return (self.__next_node)
-    
-        
+
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Represent a singly linked list"""
+
+    def __init__(self):
+        """Initialize a new singly linked list"""
+        self.__head = None
