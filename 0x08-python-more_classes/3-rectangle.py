e9d7f455888469017a200689ff2178d2b9a024c5
Pericles001 <periclesadjovi@gmail.com>
2021-12-27 17:28:58 +0100

    Add folder

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Defines a rectangle class"""
+
+from typing import Type
+
+
+class Rectangle:
+    """Represents a rectangle"""
+    
+    def __init__(self, width=0 ,height=0):
+        """Initialize a new rectangle
+        
+            Args:
+                width (int): The width of the new rectangle
+                height (int): The height of the new rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter and setter for width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter and setter for height of rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0 :
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle"""
+        return (2 *(self.__width + self.__height))
+
+    def __str__(self):
+        """Return the printable representation of the rectangle
+
+        Represents the rectangle with # character
+        """
+        if self.__width == 0 or self.__height == 0:
+            return("")
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+            return ("".join(rect))
