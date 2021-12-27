fb64d33cda993d33def389e91af0c78d1c9cba93
Pericles001 <periclesadjovi@gmail.com>
2021-12-27 16:54:24 +0100

    Update file

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,15 +1,18 @@
-"""Define a magic class matching exactly a bytecode provided by Holberton"""
+#!/usr/bin/python3
+
+"""Define a MagicClass matching exactly a bytecode provided by Holberton."""
 
 import math
 
+
 class MagicClass:
-    """Represent a circle"""
+    """Represent a circle."""
 
     def __init__(self, radius=0):
-        """Initialize a MagicClass
-        
-            Args:
-                radius (float or int): The radius of the new MagicClass
+        """Initialize a MagicClass.
+
+        Arg:
+            radius (float or int): The radius of the new MagicClass.
         """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
@@ -17,9 +20,9 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
-        """Return the area of the MagicClass"""
+        """Return the area of the MagicClass."""
         return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-        """Return the circumference of the MagicClass"""
+        """Return The circumference of the MagicClass."""
         return (2 * math.pi * self.__radius)
