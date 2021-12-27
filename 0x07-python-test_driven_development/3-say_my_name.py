756f6697b706f3a963c27a4138877b6bcd0e63c9
Pericles001 <periclesadjovi@gmail.com>
2021-12-27 14:27:05 +0100

    Update file

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -14,4 +14,3 @@ def say_my_name(first_name, last_name=""):
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
-
