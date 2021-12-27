01c524f5be48755ff8db9fabe9da63ff7aaefa85
Pericles001 <periclesadjovi@gmail.com>
2021-12-27 14:58:25 +0100

    Update files

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
-"""Module containing a function that prints a string in a certain format"""
+
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    """ Prints a string line by line based on delimiters
-        Arguments:
-        @text: text to be printed
-    """
+    """Print text with two new lines after each '.', '?', and ':'.
 
-    if type(text) is not str:
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
-    text = text.strip()
-    if len(text) == 0:
-        return
-    temp = text[0]
-    delims = [',', ':', '?']
-    for char in text:
-        if char == ' ' and temp in delims:
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
             continue
-        temp = char
-        print(char, end="")
-        if char in delims:
-            print("\n\n", end="")
+        c += 1
