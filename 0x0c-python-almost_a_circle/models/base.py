125e3b8ab7482f86e85dd8f7fa93df6437f39303
Pericles001 <periclesadjovi@gmail.com>
2021-12-28 22:35:23 +0100

    Done with task 1

--- 0x0c-python-almost_a_circle/models/base.py
+++ 0x0c-python-almost_a_circle/models/base.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
-"""Module that represent base model"""
+"""Module that represent base model class"""
 
 
 class Base:
-    """Base class"""
+    """Base model representation
+    
+        Attributes:
+            __nb__objects (int): number of bases
+    """
+
+    __nb__objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize class
+        
+            Args:
+                id (int): The identity of the new base
+        """
+
+    if id is not None:
+        self.id = id
+    else:
+        __nb__objects += 1
+        self.id = Base.__nb__objects
+
+
 
