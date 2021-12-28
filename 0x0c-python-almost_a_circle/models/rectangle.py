e44c2524aa780c3d1fb37fe426478e4bd76c4fbc
Pericles001 <periclesadjovi@gmail.com>
2021-12-28 23:50:38 +0100

    Start update function

--- 0x0c-python-almost_a_circle/models/rectangle.py
+++ 0x0c-python-almost_a_circle/models/rectangle.py
@@ -96,3 +96,18 @@ class Rectangle(Base):
                                                        self.y,
                                                        self.width,
                                                        self.height)
+
+    def update(self, *args):
+        """Method that update rectangle attributes
+        
+            Args:
+                 *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents width attribute
+                - 3rd argument represent height attribute
+                - 4th argument represents x attribute
+                - 5th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        
+        
