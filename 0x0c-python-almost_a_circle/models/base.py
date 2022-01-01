079081b336052fa7dac1686563f74841477934ba
Pericles001 <periclesadjovi@gmail.com>
2022-01-01 18:22:19 +0100

    Done with create method

--- 0x0c-python-almost_a_circle/models/base.py
+++ 0x0c-python-almost_a_circle/models/base.py
@@ -71,4 +71,21 @@ class Base:
             return []
         return json.loads(json_string)
 
+    @classmethod
+    def create(cls, **dictionary):
+        """Return a class instantied from a dictionary of attributes.
+
+        Args:
+            **dictionary (dict): Key/value pairs of attributes to initialize.
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1,1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+
     
+
+        
