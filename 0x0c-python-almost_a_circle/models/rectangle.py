5598d559faf3d30ee9bd6e770e041b98a6b5b20d
Pericles001 <periclesadjovi@gmail.com>
2022-01-01 17:30:27 +0100

    Done with first conditional structure in update function

--- 0x0c-python-almost_a_circle/models/rectangle.py
+++ 0x0c-python-almost_a_circle/models/rectangle.py
@@ -109,5 +109,21 @@ class Rectangle(Base):
                 - 5th argument represents y attribute
             **kwargs (dict): New key/value pairs of attributes.
         """
-        
-        
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+
