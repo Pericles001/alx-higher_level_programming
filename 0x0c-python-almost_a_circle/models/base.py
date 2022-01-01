de349b49de9336c221a8d2ed87e9a2e316f613b8
Pericles001 <periclesadjovi@gmail.com>
2022-01-01 18:15:53 +0100

    Done with method from_json_string

--- 0x0c-python-almost_a_circle/models/base.py
+++ 0x0c-python-almost_a_circle/models/base.py
@@ -57,3 +57,18 @@ class Base:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 jsonfile/write(Base.to_json_string(list_dicts))
 
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the deserialization of a JSON string.
+
+        Args:
+            json_string (str): A JSON str representation of a list of dicts.
+        Returns:
+            If json_string is None or empty - an empty list.
+            Otherwise - the Python list represented by json_string.
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
+
+    
