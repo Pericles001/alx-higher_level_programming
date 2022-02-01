28e9ab04cbedbe817102a790ad09ab1a5898ee10
pericles001 <periclesadjovi@outlook.fr>
2022-02-01 23:05:01 +0100

    Added instances of table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,3 +4,7 @@ CREATE TABLE IF NOT EXISTS second_table (
   name VARCHAR(256),
   score INT
 );
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(4, "George", 8);
