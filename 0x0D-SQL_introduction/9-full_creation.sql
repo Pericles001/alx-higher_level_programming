2d2b51c4ceda801193a27f4cdbfc072249c4734f
pericles001 <periclesadjovi@outlook.fr>
2022-02-01 23:02:21 +0100

    Done with task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+CREATE TABLE IF NOT EXISTS second_table (
+  id INT,
+  name VARCHAR(256),
+  score INT
+);
