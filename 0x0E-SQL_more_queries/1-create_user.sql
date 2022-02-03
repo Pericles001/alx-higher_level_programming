462aa285c405baf59d846fdd5a6ed8a6731c8fe6
pericles001 <periclesadjovi@outlook.fr>
2022-02-03 09:23:38 +0100

    Done with task

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,6 @@
 -- a script that creates the MySQL server user user_0d_1.
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES 
+ON *.*
+TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
