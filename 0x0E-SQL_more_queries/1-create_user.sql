7d8225d98ce00e7d6ec21a87e1dc6735319b1563
pericles001 <periclesadjovi@outlook.fr>
2022-02-03 09:18:38 +0100

    Update file

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- a script that creates the MySQL server user user_0d_1.
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
