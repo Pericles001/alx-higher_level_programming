fbbbca6df97d42240cade134b2801f17d82f2616
pericles001 <periclesadjovi@outlook.fr>
2022-03-01 08:16:00 +0100

    Review code file

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
 const { argv } = require('process');
-if (argv[2] === undefined && argv[3] === undefined) { console.log('Undefined is undefined'); } else if (argv[3] === undefined) { console.log(argv[2] + ' is undefined'); } else { console.log(argv[2] + ' is ' + argv[3]); }
+const argv1 = argv[2], argv2 = argv[3];
+console.log(argv1 + ' is ' + argv2);
