ec9ae6bdc549a98404c8ff6da3cba31ff8bdd1e7
pericles001 <periclesadjovi@outlook.fr>
2022-03-02 14:14:25 +0100

    Update methods

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+    constructor(w, h) {
+        if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
     }
-  }
 
-  rotate () {
-    [this.width, this.height] = [this.height, this.width];
-  }
+    print() {
+        for (let i = 0; i < this.height; i++) {
+            console.log('X'.repeat(this.width));
+        }
+    }
 
-  double () {
-    [this.width, this.height] = [this.height * 2, this.width * 2];
+    rotate() {
+        [this.width, this.height] = [this.height, this.width];
+    }
+
+    double() {
+        [this.width, this.height] = [this.width * 2, this.height * 2];
     }
 };
