e34098534f6f9c7367cdd4625bf2235f281addd7
pericles001 <periclesadjovi@outlook.fr>
2022-03-02 14:43:55 +0100

    Review code

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,20 +1,15 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  const secList = list.map(
+  list.map(
     (x) => {
       if (x === searchElement) {
+        count += 1;
         return 1;
       } else {
         return 0;
       }
     }
   );
-
-  for (let temp = 0; temp < secList.length; temp++) {
-    if (secList[temp] === 1) {
-      count += 1;
-    }
-  }
   return count;
 };
