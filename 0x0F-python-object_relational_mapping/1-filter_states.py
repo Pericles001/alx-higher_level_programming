33e6d478d1a002036ba9a555eb50a8828b7d4410
pericles001 <periclesadjovi@outlook.fr>
2022-03-06 16:15:55 +0100

    Change doc style

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# lists all states with a name starting with N (upper N)
-# from the database hbtn_0e_0_usa
-# Usage: ./1-filter_states.py <mysql username>
-#                            <mysql password>
-#                            <database name>
+""" lists all states with a name starting with N (upper N)
+ from the database hbtn_0e_0_usa
+ Usage: ./1-filter_states.py <mysql username>
+                            <mysql password>
+                            <database name>"""
 
 import sys
 from unicodedata import name
