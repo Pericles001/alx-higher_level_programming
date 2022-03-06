9a4417ccad7ee6e537cadfa7423c543baacc99e6
pericles001 <periclesadjovi@outlook.fr>
2022-03-06 16:14:23 +0100

    Change doc style

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# lists all states from the database hbtn_0e_0_usa
-# Usage: ./0-select_states.py <mysql username>
-#                            <mysql password>
-#                            <database name >
+"""lists all states from the database hbtn_0e_0_usa"""
+
 import sys
 import MySQLdb
 
