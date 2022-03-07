a1170c6418219cec95f49ea7cf27593db9a01836
pericles001 <periclesadjovi@outlook.fr>
2022-03-07 15:15:02 +0100

    remove trailing space

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("""SELECT * FROM states 
-                WHERE name LIKE BINARY '{}' 
+    c.execute("""SELECT * FROM states
+                WHERE name LIKE BINARY '{}'
                 ORDER BY states.id ASC""".format(sys.argv[4]).strip("'"))
     [print(state) for state in c.fetchall()]
