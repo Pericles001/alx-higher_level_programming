2b4e2099da4c989a88f1f6f3b8ac5824ab6f3d57
pericles001 <periclesadjovi@outlook.fr>
2022-03-07 15:45:28 +0100

    Add file

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+displays all values in the states table of
+ hbtn_0e_0_usa where name matches the argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("""SELECT * FROM STATES""")
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
