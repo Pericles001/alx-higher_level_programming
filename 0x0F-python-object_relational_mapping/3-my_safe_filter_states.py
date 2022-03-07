4bba32ea27100ba46945a9546e54ea3ce6fefde6
pericles001 <periclesadjovi@outlook.fr>
2022-03-07 15:49:07 +0100

    Review file

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,5 +10,5 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("""SELECT * FROM STATES""")
+    c.execute("""SELECT * FROM states""")
     [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
