bb1022d966c9cb4c5f75b297d04765c60b21e55f
pericles001 <periclesadjovi@outlook.fr>
2022-03-08 16:21:27 +0100

    Reduce line length

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -11,7 +11,8 @@ from model_state import State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3])
+                           ,pool_pre_ping=True)
     session_maker = sessionmaker(bind=engine)
     session = session_maker()
 
