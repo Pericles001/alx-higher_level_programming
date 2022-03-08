5bfe9abf73d88a04569d4747f82b0829523004fc
pericles001 <periclesadjovi@outlook.fr>
2022-03-08 16:36:26 +0100

    Update file

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -5,8 +5,7 @@ containing the letter a from the database hbtn_0e_6_usa
 """
 
 import sys
-from venv import create
-from sqlalchemy import create_engine, true
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
 
