389a63e7aafadd2763d0e4882c728ea08ba954a2
pericles001 <periclesadjovi@outlook.fr>
2022-03-08 16:43:24 +0100

    Update file

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -17,13 +17,13 @@ class City(Base):
     inherits from Base (imported from model_state)
     links to the MySQL table cities
 
-    class attribute id that represents a column of 
+    class attribute id that represents a column of
     an auto-generated, unique integer, can't be null and is a primary key
 
-    class attribute name that represents a column 
+    class attribute name that represents a column
     of a string of 128 characters and can't be null
 
-    class attribute state_id that represents a column 
+    class attribute state_id that represents a column
     of an integer, can't be null and is a foreign key to states.id
 
     """
