42cd8ef781f33cabafa7cdf669ebfbc2fa557588
pericles001 <periclesadjovi@outlook.fr>
2022-03-11 13:59:07 +0100

    Change code

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -26,6 +26,6 @@ class State(Base):
 
     """
     __tablename__ = "states"
-     id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state")
