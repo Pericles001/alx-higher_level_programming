5ce84a6d9fdf092308a061d8aae85e562803b49e
pericles001 <periclesadjovi@outlook.fr>
2022-03-11 13:57:40 +0100

    Change code

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -28,6 +28,6 @@ class City(Base):
 
     """
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id))
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -26,7 +26,6 @@ class State(Base):
 
     """
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+     id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship("City", backref="state")
