a42c0cc6bcabcbfdeb41c0e7ddb016e53e9f21b3
pericles001 <periclesadjovi@outlook.fr>
2022-03-22 15:37:06 +0100

    Resolve pycodestyle issue

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 a function that finds a peak in a list of unsorted integers.
 """
 
+
 def find_peak(list_of_integers):
     """
     Return a peak in a list of unsorted integers
@@ -19,6 +20,7 @@ def find_peak(list_of_integers):
         if list_of_integers[-1] > list_of_integers[-2]:
             return list_of_integers[-1]
         for i in range(1, len(list_of_integers) - 1):
-            if list_of_integers[i] > list_of_integers[i - 1] and list_of_integers[i] > list_of_integers[i + 1]:
+            if (list_of_integers[i] > list_of_integers[i - 1]
+                    and list_of_integers[i] > list_of_integers[i + 1]):
                 return list_of_integers[i]
     return None
