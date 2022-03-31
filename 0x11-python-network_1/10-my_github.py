fe6458841e5ab63bade816db1ecb8801d7008c36
pericles001 <periclesadjovi@outlook.fr>
2022-03-31 02:48:17 +0100

    Update file

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,13 +7,4 @@ import requests
 if __name__ == '__main__':
     url = 'https://api.github.com/user'
     r = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
-    try:
-        print(r.json()['id'])
-    except NameError as e:
-        print(e)
-    except KeyError as e:
-        print(e)
-    except ValueError as e:
-        print(e)
-    except Exception as e:
-        print(e)
+    print(r.json()['id'])
