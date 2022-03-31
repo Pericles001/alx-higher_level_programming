a7eef5bb6a528c214eb137aa1e19cd68592b7afd
pericles001 <periclesadjovi@outlook.fr>
2022-03-31 02:26:40 +0100

    Done with task

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email address, sends a POST request 
+to the passed URL with the email as a parameter, and finally displays 
+the body of the response.
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    r = requests.post(url, data={'email': email})
+    print(r.text)
