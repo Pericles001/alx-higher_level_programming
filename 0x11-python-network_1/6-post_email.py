9bba97439aa38f590fe46a3fadc92d7f181aab05
pericles001 <periclesadjovi@outlook.fr>
2022-03-31 02:27:21 +0100

    Review pycodestyle

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""script that takes in a URL and an email address, sends a POST request 
-to the passed URL with the email as a parameter, and finally displays 
+"""script that takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter, and finally displays
 the body of the response.
 """
 import requests
