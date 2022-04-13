53b1c7420f6d46f9fe8ee4700e465442187709dd
pericles001 <periclesadjovi@outlook.fr>
2022-04-13 16:11:29 +0100

    Update file with semistandards

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/people/18/';
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(JSON.parse(body).name);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
