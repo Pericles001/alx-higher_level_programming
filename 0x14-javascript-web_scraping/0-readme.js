b43e5cd5f660289c49e44a10b5178a78f6afbda3
pericles001 <periclesadjovi@outlook.fr>
2022-04-13 01:52:15 +0100

    Review file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, data) {
-  console.log(error || data);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
