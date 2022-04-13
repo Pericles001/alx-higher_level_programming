0e1c042cbf8fc1f38b711354974fd4fd7d9c0eef
pericles001 <periclesadjovi@outlook.fr>
2022-04-13 15:51:03 +0100

    Reviewed codebase

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+const { argv } = require('process');
+
+const BaseUrl = 'https://swapi-api.hbtn.io/api';
+request(BaseUrl + '/films/' + argv[2], (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  console.log(JSON.parse(body).title);
 });
