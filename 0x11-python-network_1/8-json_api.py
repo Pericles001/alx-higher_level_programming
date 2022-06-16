3ab4c90d0efe3ddef5923c3ba3744268ded5abcb
pericles001 <periclesadjovi@outlook.fr>
2022-06-16 16:37:49 +0100

    Update codebase

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-import requests
-import sys
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
 
-if __name__ == '__main__':
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
 
