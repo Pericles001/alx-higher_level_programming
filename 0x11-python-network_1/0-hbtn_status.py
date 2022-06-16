683995017997b4158790044796bebfcd35ab6a7b
pericles001 <periclesadjovi@outlook.fr>
2022-06-16 16:35:08 +0100

    Update codebase

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(type(response)))
         print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode('utf-8')))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
