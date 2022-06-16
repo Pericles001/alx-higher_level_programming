51d5a46252fb2fa9f43d83e76dde462272e4e0f6
pericles001 <periclesadjovi@outlook.fr>
2022-06-16 17:57:02 +0100

    Update codebase

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,11 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi.co/api/films/';
-let id = parseInt(process.argv[2], 10);
-let characters = [];
+const { argv } = require('process');
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const results = resp.results;
-    if (id < 4) {
-      id += 3;
-    } else {
-      id -= 3;
-    }
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === id) {
-        characters = results[i].characters;
-        break;
-      }
-    }
-    for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+if (argv[2] === undefined || !(Number(argv[2]))) {
+  console.log('Missing size');
+} else {
+  const rip = 'X'.repeat(argv[2]);
+  for (let i = 0; i < argv[2]; i++) {
+    console.log(rip);
   }
-});
+}
